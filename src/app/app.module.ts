77dd74a92c4b31681f0175e2e5501f8982bd05d2
alannanunez <ALANNA.NUNEZ@newhorizons.edu.do>
2021-11-29 22:54:48 +0000

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ButtonComponent } from './button/button.component';
+import { BottomComponent } from './bottom/bottom.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ButtonComponent,
+    BottomComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
